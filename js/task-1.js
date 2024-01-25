2c0eaff0e74fff3e60c6ac0514a7324373d67ed6
unknown <voroshnin.s@gmail.com>
2024-01-26 01:47:06 +0200

    Вправа 1

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ const categoriesItems = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesItems.length}`);
 categoriesItems.forEach(category => {
     console.log(`Category: ${category.querySelector('h2').textContent}`);
-    console.log(`Number of items: ${category.querySelectorAll('li').length}`);
+    console.log(`Elements: ${category.querySelectorAll('li').length}`);
 });
 
 
