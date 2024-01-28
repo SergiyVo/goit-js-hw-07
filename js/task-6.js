b5afa3ee7f3872f994a08e67d8eb28e4af65a96b
unknown <voroshnin.s@gmail.com>
2024-01-29 01:42:43 +0200

    Задача 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const myBoxes = document.getElementById('boxes');
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value);
+  createBoxes(amount);
+});
+
+function createBoxes(amount) {
+  myBoxes.innerHTML = '';
+  if (amount >= 1 && amount <= 100) {
+    let sizeBox = 30;
+    for (let i = 0; i < amount; i++) {
+      const myBox = document.createElement('div');
+      myBox.style.backgroundColor = getRandomHexColor();
+      myBox.style.width = ` ${sizeBox}px`;
+      myBox.style.height = `${sizeBox}px`;
+      myBoxes.appendChild(myBox);
+      sizeBox = sizeBox + 10;
+    }
+  };
+  input.value = '';
+};
+
+destroyButton.addEventListener('click', () => {
+  myBoxes.innerHTML = '';
+});
